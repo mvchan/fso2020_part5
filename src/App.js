417febc70f25f3f8f49606cdb9a787f9bd8bcafb
Mark Chan <mac101010@gmail.com>
2021-02-03 16:52:01 -0800

    Up to Exercise 5.4
    Added local storage functionality for logging in, blog entry creation, and notifications

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login' 
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [normalMessage, setNormalMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setURL] = useState('')
+
+  //empty array will result in only executing once on first-time render
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  //empty array will result in only executing once on first-time render
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -25,11 +42,41 @@ const App = () => {
         username,
         password
       })
+
+      //allows login to persist on re-render
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      ) 
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleBlogCreation = async (event) => {
+    event.preventDefault()
+    console.log('adding a new blog')
+
+    try {
+      const blog = await blogService.create({
+        title,
+        author,
+        url
+      })
+      setBlogs(blogs.concat(blog))
+      setNormalMessage(`a new blog titled '${blog.title}' by ${blog.author} has been added`)
+      setTimeout(() => {
+        setNormalMessage(null)
+      }, 5000)
+    } catch (exception) {
+      setErrorMessage('Could not create new blog')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -50,29 +97,39 @@ const App = () => {
     </form>
   )
 
-  /*
   const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <input
-        value={newBlog}
-        onChange={handleBlogChange}
-      />
-      <button type="submit">save</button>
+    <form onSubmit={handleBlogCreation}>
+        <div>title: <input value={title} onChange={({target}) => setTitle(target.value)} /></div>
+        <div>author: <input value={author} onChange={({target}) => setAuthor(target.value)} /></div>
+        <div>url: <input value={url} onChange={({target}) => setURL(target.value)} /></div>
+        <div><button type="submit">create</button></div>
     </form>
   )
-*/
+
   return (
     <div>
       {user === null 
         ?
         <div>
           <h2>log in to application</h2>
+          <Notification message={errorMessage} isError={true} />
           {loginForm()}
         </div> 
         :
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <Notification message={normalMessage} isError={false} />
+          <Notification message={errorMessage} isError={true} />
+          <p>{user.name} logged in 
+              <button onClick={() => {
+                    window.localStorage.removeItem('loggedBlogAppUser')
+                    setUser(null)
+                  }
+                }>logout
+              </button>
+          </p>
+          <h2>create new</h2>
+          {blogForm()}
           {blogs.filter(blog => blog.user.username === user.username).map(blog => <Blog key={blog.id} blog={blog} />)}
         </div>
       }

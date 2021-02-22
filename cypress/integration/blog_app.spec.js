b250d4a6f2fe8610ce4ca50249edff51b73a350b
Mark Chan <mac101010@gmail.com>
2021-02-21 21:04:04 -0800

    Up to Exercise 5.18
    Added login form test, including attempts with correct and incorrect credentials

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -4,65 +4,94 @@ describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
         const user = {
-            name: 'Mark',
-            username: 'mac10',
-            password: 'pass456'
+            name: 'Mark Test',
+            username: 'mac10test',
+            password: 'pass456test'
         }
         cy.request('POST', 'http://localhost:3001/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
-    it('front page can be opened', function() {
-        cy.visit('http://localhost:3000')
+    it('Login form is shown', function() {
         cy.contains('Blogs')
         cy.contains('login')
     })
 
-    it('login form can be opened', function() {
-        cy.visit('http://localhost:3000')
-        cy.contains('login').click()
-    })
-
-    it('user can log in', function() {
-        cy.contains('login').click()
-        cy.get('#username').type('mac10')
-        cy.get('#password').type('pass456')
-        cy.get('#login-button').click()
-        cy.contains('Mark logged in')
-    })
-
-    describe('when logged in', function() {
-        beforeEach(function() {
-            /*
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
             cy.contains('login').click()
-            cy.get('input:first').type('mac10')
-            cy.get('input:last').type('pass456')
+            cy.get('#username').type('mac10test')
+            cy.get('#password').type('pass456test')
             cy.get('#login-button').click()
-            */
-
-            cy.login({ username: 'mac10', password: 'pass456' })
+            cy.contains('Mark Test logged in')
         })
 
-        it('a new blog can be created', function() {
-            cy.contains('create new blog').click()
-            cy.get('#title').type('cypress blog title')
-            cy.get('#author').type('cypress blog author')
-            cy.get('#url').type('cypress blog url')
-            cy.contains('create').click()
-            cy.contains('cypress blog title')
+        it('fails with wrong credentials', function() {
+            cy.contains('login').click()
+            cy.get('#username').type('unknown')
+            cy.get('#password').type('unknown')
+            cy.get('#login-button').click()
+
+            cy.get('.error').should('contain', 'wrong username or password')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+                .and('have.css', 'border-style', 'solid')
+
+            cy.get('html').should('not.contain', 'Mark Test logged in')
         })
     })
 
-    it.only('login fails with wrong password', function() {
-        cy.contains('login').click()
-        cy.get('#username').type('mac10')
-        cy.get('#password').type('pass456')
-        cy.get('#login-button').click()
+    // it('login form can be opened', function() {
+    //     cy.visit('http://localhost:3000')
+    //     cy.contains('login').click()
+    // })
 
-        cy.get('.error').should('contain', 'wrong username or password')
-            .and('have.css', 'color', 'rgb(255, 0, 0)')
-            .and('have.css', 'border-style', 'solid')
+    // it('user can log in', function() {
+    //     cy.contains('login').click()
+    //     cy.get('#username').type('mac10')
+    //     cy.get('#password').type('pass456')
+    //     cy.get('#login-button').click()
+    //     cy.contains('Mark logged in')
+    // })
 
-        cy.get('html').should('not.contain', 'Mark logged in')
-    })
+    // describe('when logged in', function() {
+    //     beforeEach(function() {
+    //         /*
+    //         cy.contains('login').click()
+    //         cy.get('input:first').type('mac10')
+    //         cy.get('input:last').type('pass456')
+    //         cy.get('#login-button').click()
+    //         */
+
+    //         cy.login({ username: 'mac10', password: 'pass456' })
+    //     })
+
+    //     it('a new blog can be created', function() {
+    //         /*
+    //         cy.contains('create new blog').click()
+    //         cy.get('#title').type('cypress blog title')
+    //         cy.get('#author').type('cypress blog author')
+    //         cy.get('#url').type('cypress blog url')
+    //         cy.contains('create').click()
+    //         cy.contains('cypress blog title')
+    //         */
+    //         cy.createBlog({
+    //             title:'cypress blog title',
+    //             author:'cypress blog author',
+    //             url:'cypress blog url'
+    //         })
+    //     })
+    // })
+
+    // it.only('login fails with wrong password', function() {
+    //     cy.contains('login').click()
+    //     cy.get('#username').type('mac10')
+    //     cy.get('#password').type('pass456')
+    //     cy.get('#login-button').click()
+
+    //     cy.get('.error').should('contain', 'wrong username or password')
+    //         .and('have.css', 'color', 'rgb(255, 0, 0)')
+    //         .and('have.css', 'border-style', 'solid')
+
+    //     cy.get('html').should('not.contain', 'Mark logged in')
+    // })
 })
